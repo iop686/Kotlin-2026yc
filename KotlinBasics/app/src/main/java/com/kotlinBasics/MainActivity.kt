66174f9154f782f2b8ee0b44e6b4a2422cb7a354
unknown <iop060806@naver.com>
2026-09-14 16:27:19 +0900

    update files from professor

--- KotlinBasics/app/src/main/java/com/kotlinBasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinBasics/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinBasicsTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -28,20 +28,66 @@ class MainActivity : ComponentActivity() {
             }
         }
         week03Variables()
+        week03Functions()
     }
 }
 
 private fun week03Variables() {
-    println("Week 03 : Variables")
+    println("Week 03: Variables")
 
-    val courseName = "Mobile Programming"  // Java final keyword
-    //courseName = "DataStructure"  // error
+    val courseName = "Mobile Programming"  // java final
+    // courseName = "Data Structure"  // error
 
     var week = 2
     week = 3
     println("Course : $courseName")
     println("Week : $week")
+
+    println("========= Kotlin Variables =========")
+
+    // val(immutable) vs var(mutable)
+    val name = "Android"
+    var version = 8
+
+    println("Hi $name $version")
+
+    val age: Int = 24
+    val height: Double = 177.7
+    val isStudent: Boolean = false
+
+    println("Age: $age, Height: $height, Student: $isStudent")
+
+//    var nickname:String = null
+    var nickname:String? = null
+    nickname = "mirae"
+    println("Nickname: $nickname ${nickname?.length}")
 }
+
+private fun week03Functions(){
+//    println("Week 03: Functions")
+//
+//    fun greet(name: String) = "Hello, $name!"
+//
+//    println(greet("Android developer"))
+
+    println("== Kotlin Functions ==")
+
+    fun greet(name: String): String {
+        return "Hello, $name!"
+    }
+
+    fun add(a: Int, b: Int) = a + b
+
+    fun introduce(name: String, age: Int = 19){
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum: ${add(5, -71)}")
+    introduce("Kim", 7)
+    introduce("Park")
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
